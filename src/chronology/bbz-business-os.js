d2a4f3d64dc42ebc8e7c0a58b707a1f303494667
Rob <rmoskal@mostmedia.com>
2022-03-10 21:19:47 -0500

    go

--- src/chronology/bbz-business-os.js
+++ src/chronology/bbz-business-os.js
@@ -9,15 +9,15 @@ module.exports = {
   roles: ['principal', 'leader'],
   long:true,
   body: `
-  This "startup" within PWC moved rapidly to create a new generation of software for field service and retail sales organizations. The goal was to replace behemoths like Great Plains and home grown systems with modern applications that utilized technologies such as big data, machine learning, and conversational interfaces.
+  This "startup" within PWC moved rapidly to create a new generation of software for field service and retail sales organizations. The goal was to replace behemoths like Great Plains and home grown systems with modern applications that use  big data, machine learning, and conversational interfaces.
 
   Initially, I was to identify and enforce commonalities across the efforts of a large and fractious group of vendors.
   
-  I reviewed code bases and dev efforts, evangelizing for a common set of practices around the development of micro-services, continuous integration/deployment, containerization, search, ETL, and the use of BPM engines for the orchestration and business logic.
+  I onboarded teams, reviewed code bases, and evangelizing for a common set of practices around the development of micro-services, continuous integration/deployment, containerization, search, ETL, and the use of BPM engines for the orchestration and business logic.
   
   It soon became apparent that I was only adding to the cacophony in that role. 
   
-  I  withdrew into one of the  product teams under the rubric of "fit-bit for work". As the new apps gathered ever increasing amounts of data we would provide individuals and management tools for meaningfully changing behavior in ways that improved organizational performance. Working with my friends at <a target="_blank" href="https://infobeans.com/philosophie/">Philosophie</a>, I got a taste of their flavor of agile, as we designed, developed, validated, and piloted server several iterations of the app.
+  I  withdrew into one of the  product teams working under the rubric of "fit-bit for work". The apps would gather an ever increasing amount of data; we would provide individuals and management tools for meaningfully changing behavior in ways that improved organizational performance. Working with my friends at <a target="_blank" href="https://infobeans.com/philosophie/">Philosophie</a>, I got a taste of their flavor of agile, as we designed, developed, validated, and piloted server several iterations of the app.
   
     
   `
