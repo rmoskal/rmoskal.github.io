f44ee2ab7924eababeb0ba7ca201236c10bc7f57
rob <rmoskal@mostmedia.com>
2022-03-17 16:05:23 -0400

    fooo

--- src/chronology/ba-agt.js
+++ src/chronology/ba-agt.js
@@ -11,13 +11,14 @@ module.exports = {
 
   My involvement with AGT began with an gift/experiment. Could tech in Itchy Knows, my hashtag aggregator improve SEO for this sprawling directory site that lives and dies by organic search performance.
 
-  Soon I was doing a lot more. I felt inclined to help AGT in much the same way children want to nurse an injured bird back to health. The codebase was crippled by technical debt and outright abuse accrued over a dozen years, and was riddled with security holes.
+  Soon I was doing a lot more. I felt inclined to help AGT in much the same way children want to nurse an injured bird back to health. The codebase was crippled by technical debt and outright abuse accrued over a dozen years. It was riddled with security holes.
 
-  I've worked with the founder to open up new lines of business and have  to lean startup practices like experimenting via minimal "build, measure, learn" iterations.  
+  With my own two hands and with help we've managed rehabilitate it. I've worked with the founder to open up new lines of business and to adopt lean practices like experimenting via minimal "build, measure, learn" iterations.  
+
+  I myself have become schooled in the dark arts of SEO, pretty good against defending a busy site against attackers, and have become a tolerably good PHP programmer (if there were such a thing as idiomatic php, I don't think I write it).
   
-  I myself have become schooled in the dark arts of SEO, pretty good against defending a busy site against attackers,  and have become a tolerably good PHP programmer (if there were such a thing as idiomatic php, I don't think mine would be considered so)
-.
-  I've worked with a number of off-shore teams to modernize the application. It's now a pretty standard php MVC application, but just large enough where it's too much to be maintained by a couple of solo practitioners.
+  It's now a pretty solid php MVC application, but just large enough where it's too much to be maintained by a couple of solo practitioners.
+
 
 This year, I've begun transitioning routine development for the site to a new off-shore partner, and will continue my relationship with the founder as an advisor.
   
