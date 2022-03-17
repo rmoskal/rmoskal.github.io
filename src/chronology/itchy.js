2222c2d99b3d9d8a258bf301f3e784d60d520486
rob <rmoskal@mostmedia.com>
2022-03-17 16:55:52 -0400

    push

--- src/chronology/itchy.js
+++ src/chronology/itchy.js
@@ -6,8 +6,7 @@ module.exports = {
   stack:['B2C', '<i class="fa-brands fa-python"></i>'],
   roles: [ 'founder', 'market-fit'],
   body: `
-  I started coding Itchy about the same time everyone else started working on hash tag aggregators <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/">else</a>, maybe a few days earlier.
-
+  I started coding Itchy about the same time everyone <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/">else</a> started working on hash tag aggregators, maybe a few days earlier.
 
 I actively pursued the the project for two years. When I shuttered it, I left behind several hundred dissapointed users.
 
