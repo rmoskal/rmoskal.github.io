198f0acb00e11c32146c4f5d4371f7b5f3b4a5ed
Rob <rmoskal@mostmedia.com>
2022-03-16 20:54:26 -0400

    poot

--- src/chronology/itchy.js
+++ src/chronology/itchy.js
@@ -6,14 +6,10 @@ module.exports = {
   stack:['B2C', '<i class="fa-brands fa-python"></i>'],
   roles: [ 'founder', 'market-fit'],
   body: `
-  I started coding Itschy about the same time everyone else started working on hash tag aggregators <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/">else</a> did, maybe a bit earlier.
-
-  The 
+  I started coding Itchy about the same time everyone else started working on hash tag aggregators <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/">else</a>, maybe a few days earlier.
 
 
-  I started coding itchy almost to the day that the Paul Berry, the Huffington Post CTO, started building Rebel Mouse, the social media publishing platform that started out as a hashtag aggregator.
-
-I actively pursued the the project for two years. In its current incarnation itchy is used for sharing hash tagged images in instagram, flickr, and twitter, usually for events. Here is an example: The Great Pupkin.
+I actively pursued the the project for two years. When I shuttered it, I left behind several hundred dissapointed users.
 
 I've been able to reuse large parts of the technology for a number of projects, icluding SEO enhancements for agreatertown.
 
