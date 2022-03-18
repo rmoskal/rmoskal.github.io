7855fd007d0f30d3013692745c39b201c1b8404d
rob <rmoskal@mostmedia.com>
2022-03-17 21:18:32 -0400

    itchy

--- src/chronology/itchy.js
+++ src/chronology/itchy.js
@@ -6,11 +6,12 @@ module.exports = {
   stack:['B2C', '<i class="fa-brands fa-python"></i>'],
   roles: [ 'founder', 'market-fit'],
   body: `
-  I started coding Itchy about the same time everyone <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/">else</a> started working on hash tag aggregators, maybe a few days earlier.
-
-I actively pursued the the project for two years. When I shuttered it, I left behind several hundred dissapointed users.
-
-I've been able to reuse large parts of the technology for a number of projects, icluding SEO enhancements for agreatertown.
+  Thinking there was value and perhaps opportunity in bringing some order to the chaotic world of social media, I started coding Itchy when everyone else started working on <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/"> on social media aggregators </a>.
+  <img class="center" src="/images/itchy.gif"/>
+  Folks liked it and used it. They would create <b>#hashtags</b> for events like proms, festivals, corporate meetings, etc. Itchy would monitor facebook, twitter, and instagram for images with hashtag and shunt them to a facebook page, a tumblr, or a custom website.
+   I worked on Itchy for two years. I would have continued, but had to shutter it when instagram discontinued it's hashtag search API which made a big chunk of the internet go dark. Itchy left behind several hundred disappointed users.
+Itchy marked my first big python based system.  The python eco-system is cushy enough, but I find coding in python not to taste.  I'm glad I was able to pivot to node/javascript about the same time.
+Fortunately I was able to reuse large parts of the Itchy on a number of studio projects, including SEO enhancements for agreatertown.
 
     
   `
