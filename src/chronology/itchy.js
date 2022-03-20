66489b13d9111c463dff666fcdd566cc69cf00cb
rob <rmoskal@mostmedia.com>
2022-03-20 12:56:51 -0400

    go go

--- src/chronology/itchy.js
+++ src/chronology/itchy.js
@@ -6,7 +6,7 @@ module.exports = {
   stack:['B2C', '<i class="fa-brands fa-python"></i>'],
   roles: [ 'founder', 'market-fit'],
   body: `
-  Thinking there was value and perhaps opportunity in bringing some order to the chaotic world of social media, I started coding Itchy when everyone else started working on <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/"> on social media aggregators </a>.
+  Thinking there was value and perhaps a business opportunity in bringing some order to the chaotic world of social media, I started coding Itchy when everyone else started working on <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/"> on social media aggregators </a>, maybe a few days earlier.
   <img class="center" src="/images/itchy.gif"/>
   Folks liked it and used it. They would create <b>#hashtags</b> for events like proms, festivals, corporate meetings, etc. Itchy would monitor facebook, twitter, and instagram for images with hashtag and shunt them to a facebook page, a tumblr, or a custom website.
    I worked on Itchy for two years. I would have continued, but had to shutter it when instagram discontinued it's hashtag search API which made a big chunk of the internet go dark. Itchy left behind several hundred disappointed users.
--- src/chronology/xa-adventshare.js
+++ src/chronology/xa-adventshare.js
@@ -1,24 +1,16 @@
 module.exports = {
   title:`adventshare`, 
   link:'',
-  when:'2003 – 2004',
+  when:'2003 – 2005',
   stack:['B2C', '<i class="fa-brands fa-java"></i>'],
   roles: [ 'founder'],
   long:true,
   body: `
-  Like instagram, but for extreme sports practitioners, and 7 years too early (no really!).
+Like instagram, but for extreme sports practitioners, and 7 years too early (no really!).
+  
+Drawing on lessons learned over five years of server side java development, I'd put in place sort of service oriented stack that companies like Airbnb, Spotify, and Uber still use in 2022.  
 
-  It needed to be bullet proof and able to support potentially thousands of simultaneous users. It needed a flexible architecture that could evolve in response to new opportunities.
-  
-  I built it using the J2EE application stack, putting into play the lessons learned over five years of server side java development.
-  
-  I selected a light-weight version of the J2EE, eschewing the use of entity beans entirely. Instead the Hibernate framework was used for object-relational mapping. Much of the dynamic content is generated using XSL transformations, but only when the underlying data changed. Serving “static” content in this way garnered huge performance increases.
-  
-  Application components were loosely connected using the java messaging service. This application service bus provided a great deal of flexibility in adding new features, and also in provisioning and deployment. Finally, a test driven development methodology and the appropriate use of design patterns ensured even deep changes could be made safely and quickly.
-  
-  The application was linked to Paypal for e-commerce functionality, with photo printing services provided by Club Photo. Integration was using the REST protocol.
-  
+I marketed this hard to adventure travel companies for 3 years, but it turns out that I was way, way ahead of the tech adoption curve. On the plus side this tech stack and style of development served me well for almost s decade. Plus I got tax deductions for trips to Costa Rica, the Dominican Republic, and British Columbia.  
 
-    
   `
 }
