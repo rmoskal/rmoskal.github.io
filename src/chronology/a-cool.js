ae483cb9a4e6ac667b48631343d592a054cc0a08
rob <rmoskal@mostmedia.com>
2022-03-31 10:30:20 -0400

    betterer

--- src/chronology/a-cool.js
+++ src/chronology/a-cool.js
@@ -8,8 +8,10 @@ module.exports = {
   roles: ['principal' ],
   long:true,
   body: `
-Created a cms and publishing system for "long tail" content that constitutes the institutional memory for Foundation for Advancement in Conservation (FAIC). cOOL is an archive of some quarter of a million documents that include web sites, journals, and email lists dating back to the 1980s. It's an authoritative and invaluable resource for museum conservators from around the world.
+Created a cms and publishing system for the "long tail" content that is the institutional memory for Foundation for Advancement in Conservation (FAIC). cOOL is an archive of some quarter of a million documents comprised of web sites, journals, and email lists dating back to the 1980s. It's an authoritative and invaluable resource for museum conservators from around the world.
 
-This is here because it's the sort of thing thing it's worth contacting me for and is representative of the way I work in 2019.  I used open source software and the cloud to solve a problem simply and elegantly while along the way giving improving two of the open source tools I used: <a href="https://github.com/pgherveou/gulp-awspublish" target="_blank">gulp-awspublish</a> and <a href="https://github.com/etylsarin/gulp-ssi"  target="_blank">gulp-ssi</a>
+It's described here because it's the sort of thing worth engaging me for and is representative of the .  I used free and open source software and the cloud to solve a problem simply and elegantly while along the way giving back improving the code I used.: <a href="https://github.com/pgherveou/gulp-awspublish" target="_blank">gulp-awspublish</a> and <a href="https://github.com/etylsarin/gulp-ssi"  target="_blank">gulp-ssi</a>
+
+The publishing system is based on aws hosted git, a CI/CD pipeline in CloudBuild,  with CloudFront and lambda edge functions serving contents from an S3 bucket. As FAIC personnell  can not only work with content easilu
   `
 }
--- src/chronology/cc-frockhub.js
+++ src/chronology/cc-frockhub.js
@@ -3,18 +3,16 @@ module.exports = {
   link:'https://frockhub.herokuapp.com/#/index',
   when:'2015 – 2106 ',
   stack:['B2C', 
-  '<img alt="elasticsearch" title="elasticsearch" style="margin-Top:-3px" src="/images/elastic-elasticsearch.svg" width="16px"/>', 
   '<img alt="python" title="python" style="margin-Top:-3px" src="/images/python.svg" width="16px"/>', 
-  '<img alt="javascript" title="javascript" style="margin-Top:-3px" src="/images/js.svg" width="16px"/>'],
+  '<img alt="javascript" title="javascript" style="margin-Top:-3px" src="/images/js.svg" width="16px"/>',
+  '<img alt="elasticsearch" title="elasticsearch" style="margin-Top:-3px" src="/images/elastic-elasticsearch.svg" width="16px"/>'],
   roles: ['principal', 'turn-around', 'leader'],
   long:true,
   body: `
-  I led the turn-around of the development of this high fashion/micro-blogging application. The existing code base consisted of bits and pieces of technology with partial and often ill thought out attempts at integration.
+  I led the turn-around for the build of this high fashion/micro-blogging application. 
 
-  Worked with a competent CTO to quickly assemble a development team. We decided to continue development of a hybrid mobile application using ionic, python to scrape product information, and node for a RESTFUL api. We replaced a home-grown full text search system with elasticsearch. 
+  Finding bits and pieces of technology with partial and often ill thought out attempts at integration, the CTO and quickly assembled a new team and moved forward with  a hybrid mobile application using ionic, python to scrape product information, and node for a RESTFUL api. We replaced a home-grown full text search system with elasticsearch. 
 
-  Initially, I supervised both the python developers coding the scrapers and built out the development/deployment infrastructure. As the team callesced,   I was able to focus on building out the back end domain model and api as well as evolving the the product direction with the management team. Finally, I directed the work of an off-shore data science team to put in place the product search using ES.
-  
-    
+  Initially, I supervised both the python developers coding the scrapers and built out the development/deployment infrastructure. As the team coalesced, I was able to focus on building out the back end domain model and api as well as evolving the the product direction with the management team. Finally, I directed the work of an off-shore data science team to put in place the product search using ES.   
   `
 }
