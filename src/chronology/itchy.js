818e13e97cece0f174900f09ae14f7145386d4ff
rob <rmoskal@mostmedia.com>
2022-05-17 20:47:31 -0400

    soright

--- src/chronology/itchy.js
+++ src/chronology/itchy.js
@@ -6,11 +6,11 @@ module.exports = {
   stack:['B2C',   '<img alt="python" title="python" style="margin-Top:-3px" src="/images/python.svg" width="16px"/>'],
   roles: [ 'founder', 'market-fit'],
   body: `
-  Thinking there was value and perhaps a business opportunity in bringing some order to the chaotic world of social media, I started coding Itchy when everyone else started working on <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/"> on social media aggregators </a>, maybe a few days earlier.
+  Thinking there was value and perhaps a business opportunity in bringing some order to the chaotic world of social media, I started coding Itchy when everyone else started working on <a target= "_blank" href="https://www.flatheadenterprises.com/seo/hashtags-tint-vs-tagboard-vs-twubs-vs-postano-vs-rebel-mouse/"> on social media aggregators </a>, maybe a few weeks earlier.
   <img class="center" src="/images/itchy.gif"/>
-  Folks liked it and used it. They would create <b>#hashtags</b> for events like proms, festivals, corporate meetings, etc. Itchy would monitor facebook, twitter, and instagram for images with hashtag and shunt them to a facebook page, a tumblr, or a custom website.
-   I worked on Itchy for two years. I would have continued, but had to shutter it when instagram discontinued it's hashtag search API which made a big chunk of the internet go dark. Itchy left behind several hundred disappointed users.
-Itchy marked my first big python based system.  The python eco-system is cushy enough, but I find coding in python not to taste.  I'm glad I was able to pivot to node/javascript about the same time.
+  Folks liked it and used it. They would create <b>#hashtags</b> for events like proms, festivals, corporate meetings, etc. Itchy would monitor facebook, twitter, and instagram for images with the tags and shunt them to a facebook page, a tumblr, or a custom website.
+   I worked on Itchy for two years. I would have continued, but had to shutter it when instagram discontinued it's public search API which made a big chunk of the internet go dark. Itchy left behind several hundred disappointed users.
+Itchy marked my first big python based system.  The python eco-system is cushy enough, but I find coding in the language not to my tastes.  I'm glad I was able to pivot to node/javascript about the same time.
 Fortunately I was able to reuse large parts of the Itchy on a number of studio projects, including SEO enhancements for agreatertown.
 
     
