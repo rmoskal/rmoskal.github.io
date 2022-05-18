26cd34349ce57c4e807e8a784fbb0e3852b57b8b
rob <rmoskal@mostmedia.com>
2022-05-17 23:05:44 -0400

    futon

--- src/chronology/ba-agt.js
+++ src/chronology/ba-agt.js
@@ -11,15 +11,15 @@ module.exports = {
   long:true,
   body: `
 
-  My involvement with AGT began with an gift/experiment. Could tech in Itchy Knows, my hashtag aggregator improve SEO for this sprawling directory site that lives and dies by organic search performance.
+  My involvement with AGT began with a gift/experiment. Could tech in Itchy Knows, my hashtag aggregator improve SEO for this sprawling directory site that lives and dies by organic search performance.
 
   Soon I was doing a lot more. I felt inclined to help AGT in much the same way children want to nurse an injured bird back to health. The codebase was riddled with security holes and crippled by technical debt and outright abuse accrued over a dozen years.
 
-  With my bare hands and with help we've managed rehabilitate it and now it's a solid PHP MVC app. I've worked with the founder to open up new lines of business and to adopt lean practices like experimenting via minimal "build, measure, learn" iterations.  
+  With my bare hands and with help from dev partners we've managed rehabilitate it and now it's a solid PHP MVC app. I've worked with the founder to open up new lines of business and to adopt lean practices like experimenting via minimal "build, measure, learn" iterations.  
 
-  I myself have become schooled in the dark arts of SEO, pretty good at defending a busy site against attackers, and am now a competent PHP programmer (though if there's such a thing as idiomatic php, I don't think I write it).
+  Along the way, I've become schooled in the dark arts of SEO, pretty good at defending a busy site against attackers, and am now a competent PHP programmer (though if there's such a thing as idiomatic php, I don't think I write it).
   
-  AGT just large enough where it needs to maintained and evolved by an organization and a not a couple of solo practitioners. This year, I've begun transitioning  development to a new partner, and will continue my relationship with the founder as an advisor.
+  AGT is just large enough where it needs to maintained and evolved by an organization and a not a couple of solo practitioners. This year, I've begun transitioning  development to a new partner, and will continue my relationship with the founder as an advisor.
   
   `
 }
