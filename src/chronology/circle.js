3a0532cbf1b20f7c88e95c2cb9ec3a07c4b73232
rob <rmoskal@mostmedia.com>
2022-05-18 10:09:29 -0400

    tora

--- src/chronology/circle.js
+++ src/chronology/circle.js
@@ -10,9 +10,11 @@ module.exports = {
   body: `
   I led the engineering effort for this social re-commerce marketplace. Think craigslist 3.0 where community and transparency are valued above anonymity.
 
-  The cir.cl archtecture builds on what I've learned on the Exorcists VS Demons RPG and is my second iteration on a large scale full-stack javascript application. We use express and backbone. Mongo, postgres, redis, and neo4j are our data-stores. The application makes extensive use of reactive techniques like out of request loop messaging, and event sourcing.
+  The cir.cl architecture builds on what I've learned on the Exorcists VS Demons RPG and is my second iteration on a large scale full-stack javascript application. We use express and backbone. Mongo, postgres, redis, and neo4j are our data-stores. The application makes extensive use of reactive techniques like out of request loop messaging, and event sourcing.
   
   Working along with a young engineering team, we quickly executed on the founder's vision without accruing technical debt.
+
+  Bonus fact! The angel investors for circ.cl were Eric Schmidt and the rock band Metallica.  We took meetings with both, but never at the same time! 
     
   `
 }
