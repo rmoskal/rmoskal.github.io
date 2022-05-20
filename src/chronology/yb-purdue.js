d59c712c2efa3e985c3c2d75a6693faf5e0ab5de
rob <rmoskal@mostmedia.com>
2022-05-20 10:23:08 -0400

    push

--- src/chronology/yb-purdue.js
+++ src/chronology/yb-purdue.js
@@ -8,13 +8,13 @@ module.exports = {
   '<i class="fa-brands fa-microsoft"></i>', '<i title="java" class="fa-brands fa-java"></i>'],
   roles: ['principal', 'transformation'],
   body: `
-  Carried out custom application development for the research and development division of this pharmaceutical company along with a team I put together.
+  Carried out custom application development for the research and development division of this pharmaceutical company along with a team I assembled.
 
   I originally won the business away from a poorly performing vendor and carried forward work for which internal IT resources were ill-suited.
   
   Our collaboration grew into a suite of some half dozen enterprise applications (with the technology split between J2EE and .net applications). These included project management applications (including work flow, calendaring, and document management), business intelligence dashboards, and payment and clinical trial management systems. Most notable was project.pharma, the system of record for the firm's drug development pipeline and a clinical investigator payment application that ran uninterruptedly for some 8 years.
   
-  Towards the end of the engagement, we began moving much of the suites features into SharePoint. When the company decided to move away from bespoke applications, we lead the transition and so engineered ourselves out of an engagement.
+  Towards the end of the engagement, we began moving much of the suites features into SharePoint. When the company decided to move away from bespoke applications, we led the transition and engineered ourselves out of the relationship.
   
   `
 }
