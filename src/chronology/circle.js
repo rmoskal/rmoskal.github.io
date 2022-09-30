7e682ef7322e026c4d4a84d208814417e575389e
rob <rmoskal@mostmedia.com>
2022-09-30 14:26:09 -0400

    cool new feature

--- src/chronology/circle.js
+++ src/chronology/circle.js
@@ -14,6 +14,8 @@ module.exports = {
   
   Working along with a young engineering team, we quickly executed on the founder's vision without accruing technical debt.
 
+  With this project I realized that solid tech, a strong team, and even what seems like a great product is NEVER enough to ensure success. One has to make do with small bets (some call them experiments), learn from them, and iterate.
+
   Bonus fact! The angel investors for circ.cl were Eric Schmidt and the rock band Metallica.  We took meetings with both, but never at the same time! 
     
   `
