4e23272dfcf1b2e4d2015e0a707ca48bcd74e595
rob <rmoskal@mostmedia.com>
2022-12-21 15:41:06 -0500

    hey ho

--- src/chronology/cd-shortawards.js
+++ src/chronology/cd-shortawards.js
@@ -6,6 +6,9 @@ module.exports = {
   roles: ['principal'],
   long:true,
   body: `
+
+This is an example of the sort of work I did around the time where I would work as an extra hand helping out a team with an established code base. It also shows that I know  a fair amount about  django, though in fact I don't care for nanny frameworks.
+
 I merged the code for the agencies and individuals awards programs into one and helped to create a third contest type: Shorty Awards for Social Good.
 
 I also worked a bit on evolving the data model and administrative interface for their other business: <a href="https://muckrack.com">https://muckrack.com</a>. Primarily, I made fancy tweaks to the django administrative interface.
