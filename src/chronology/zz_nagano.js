a4956afd20a7cdb04a17c79699bdd7235a0d7774
rob <rmoskal@mostmedia.com>
2023-10-13 12:25:39 -0400

    hey ho

---
+++ src/chronology/zz_nagano.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title:`Athlete Id Generation Printing System, Nagano Winter Olympics`, 
+  link:null,
+  when:'1997 – 1998',
+  stack:['B2B'],
+  roles: ['principal'],
+  body: `
+The only sad thing about this project is that it worked so well during testing that I wasn't asked  on-site to baby-sit it.    
+  `
+}
