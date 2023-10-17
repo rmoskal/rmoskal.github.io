3a77b253f75ffa34f4ffcc4cd8211c7bea1537c8
rob <rmoskal@mostmedia.com>
2023-10-17 14:44:30 -0400

    tora topra

--- src/chronology/zz_nagano.js
+++ src/chronology/zz_nagano.js
@@ -1,10 +1,11 @@
 module.exports = {
-  title:`Athlete Id Generation Printing System, Nagano Winter Olympics`, 
+  title:`Athlete Photo Id System, Nagano Winter Olympics`, 
   link:null,
   when:'1997 – 1998',
   stack:['B2B'],
   roles: ['principal'],
   body: `
+  Digital cameras and custom hardware builds back when it was scary.
 The only sad thing about this project is that it worked so well during testing that I wasn't asked  on-site to baby-sit it.    
   `
 }
