bc046b3ebb698d4c3942bdaaad0fef7de89b6cdd
rob <rmoskal@mostmedia.com>
2023-11-12 13:42:47 -0500

    add the argus

---
+++ src/chronology/13-argus.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title:`CollectionSpace to Argus Migration`, 
+    link:'https://www.controlboard.app/',
+    when:'2023',
+    stack:['ETL']
+  ,
+    roles: [ 'principal'],
+    long:false,
+    body: `
+    Migrated ALL the collection data for the  <a href="https://museumofus.org/" target="_blank">Museum of Us</a> from <a href="https://collectionspace.org/" target="_blank">CollectionSpace</a> to <a href="https://lucidea.com/argus/" target="_blank">Lucidea's Argus</a>.
+    
+    Give a shout if you need this done. I've got the tools to make it easy.  
+    `
+  }
