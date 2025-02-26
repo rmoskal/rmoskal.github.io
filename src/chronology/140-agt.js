035786e9779d8edd23a641a5fa82ae69e0734131
rob <rmoskal@mostmedia.com>
2025-02-26 14:09:15 -0500

    update the drew story

--- src/chronology/140-agt.js
+++ src/chronology/140-agt.js
@@ -11,15 +11,15 @@ module.exports = {
   long:true,
   body: `
 
-  My involvement with AGT began with a gift/experiment. Could tech in Itchy Knows, my hashtag aggregator improve SEO for this large scale (over 10 million pages) directory site.
+  My involvement with AGT began with a gift/experiment. Could tech behind Itchy Knows, my hashtag aggregator, improve SEO for a directory site with more than 10 million pages.
 
   Soon I was doing a lot more. My clients ability to progress and adapt was ham-strung by a legacy code-base that had been worked over by many hands over the course of a dozen years. I felt compelled to help.
 
-  It's now a modern and solid PHP MVC app running on the google cloud. I've worked with the founder to open up new lines of business and to adopt lean practices like experimenting via minimal "build, measure, learn" iterations.  
-
+  Working on my own and directing the efforts of a series of development partners it's now a modern and evolvable set of applications in PHP and python/apache beam for etl.
+  
   Along the way, I've become schooled in issues that arise when you have a statistically significant number of web pages (especially with regard to SEO), pretty good at defending a busy site against attackers, and am now a competent PHP programmer (though if there's such a thing as idiomatic php, I don't think I write it).
   
-  AGT is large enough where it needs to maintained and evolved by an organization and a not a couple of solo practitioners. In 2022, I've begun transitioning development to a new development partner, and will continue my relationship with agt as a fractional CTO.
+  Day to today operation of AGT is now in the hands of a team at <a target= "_blank" href="https://august99.com/">August 99</a> who are also strategic partner as Drew pivots into proptech. I continue as an advisor and fractional CTO.
   
   `
 }
