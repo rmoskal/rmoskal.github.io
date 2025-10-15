47840bb2d2d980260ecde74f184856837f31f88b
rob <rmoskal@mostmedia.com>
2025-10-15 10:26:29 -0400

    hey ho

--- src/chronology/135-science.js
+++ src/chronology/135-science.js
@@ -3,11 +3,13 @@ module.exports = {
   link:'https://apps.apple.com/us/app/playground-physics/id947124790',
   when:'2019 – 2020',
   stack:['fCTO, Product'],
-  roles: ['fCTO, ' ],
-  long:false,
+  roles: ['fCTO' ],
+  long:true,
   body: `
+
   First I led the development effort of Playground Physics app for the NY Hall of Science along with my friends at <a href="https://www.bluetelescope.com/" target="_blank"> Blue Telescope</a>. We used am off-shore development team.
 
   After launch I mentored the Hall of Science team through the Verizon 5G incubator. There I mediated between the language and expectations of an established educational institution with a history of innovation and the culture of a tech incubator.
+  
   `
 }
